82500ce03e3e73a8cb14a3864a0c0ce10cfe2297
juyeop <shininggiver@gmail.com>
2021-04-05 11:44:34 +0900

    [feat/getComments/completed][모든 유저의 의견 데이터 받아오기 기능 구현 완료]

--- controllers/OpinionController.js
+++ controllers/OpinionController.js
@@ -1,8 +1,34 @@
 const { candidate, opinion, election, user } = require('../models');
+// const { delete } = require('../routes/OpinionRouter');
 
 module.exports = {
     getComments: async (req, res) => {
-        res.send('getComments')
+        opinion.findAll()
+            .then(rawOpinions => {
+                if (rawOpinions.length === 0) {
+                    res.status(200).json({
+                        comments: []
+                    })
+                } else {
+                    return rawOpinions.map(rawOpinion => rawOpinion.dataValues)
+                }
+            })
+            .then(opinions => {
+                const comments = opinions.map(opinion => {
+                    delete opinion.updatedAt;
+                    return opinion
+                })
+                res.status(200).json({
+                    comments
+                })
+            })
+            .catch(err => {
+                if (err) {
+                    res.status(500).json({
+                        message: "Internal server error!"
+                    })
+                }
+            })
     },
     postComment: async (req, res) => {
         res.send('postComment')
