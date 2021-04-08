9929294e2dfb17efc3ddbc82c81f3d73f96bbd3b
juyeop <shininggiver@gmail.com>
2021-04-08 15:52:27 +0900

    [feat/getCandidates/completed][선거별 후보자의 목록 받아오기]

---
+++ controllers/promiseController.js
@@ -0,0 +1,78 @@
+const axios = require('axios')
+const dotenv = require('dotenv');
+dotenv.config()
+const convert = require('xml-js');
+const request = require('request');
+
+module.exports = {
+    getCandidates: async (req, res) => {
+        let { sgId, sgTypecode } = req.body
+        if (sgId && sgTypecode) {
+            let url = 'http://apis.data.go.kr/9760000/PofelcddInfoInqireService/getPofelcddRegistSttusInfoInqire';
+            let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + process.env.SERVICEKEY; /* Service Key*/
+            queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+            queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
+            queryParams += '&' + encodeURIComponent('sgId') + '=' + encodeURIComponent(sgId); /* */
+            queryParams += '&' + encodeURIComponent('sgTypecode') + '=' + encodeURIComponent(sgTypecode); /* */
+            request({
+                url: url + queryParams,
+                method: 'GET'
+            }, function (error, response, body) {
+                const placesJson = JSON.parse(
+                        convert.xml2json(body, {
+                        compact: true,
+                        spaces: 4
+                    })
+                )
+                if (placesJson.response.body === undefined) {
+                    res.status(500).json({
+                        message: 'Internal server error'
+                    })
+                } else {
+                    res.status(200).json(placesJson.response.body.items.item)
+                }
+            });
+        } else {
+            res.status(404).json({
+                message: 'post correct info'
+            })
+        }
+    },
+    getPromises: async (req, res) => {
+        let { sgId, sgTypecode, cnddtId } = req.body;
+        if (sgId && sgTypecode && cnddtId) {
+            let url = 'http://apis.data.go.kr/9760000/ElecPrmsInfoInqireService/getCnddtElecPrmsInfoInqire';
+            let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + process.env.SERVICEKEY;
+            queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+            queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+            queryParams += '&' + encodeURIComponent('sgId') + '=' + encodeURIComponent(sgId); /* */
+            queryParams += '&' + encodeURIComponent('sgTypecode') + '=' + encodeURIComponent(sgTypecode); /* */
+            queryParams += '&' + encodeURIComponent('cnddtId') + '=' + encodeURIComponent(cnddtId); /* */
+            request({
+                url: url + queryParams,
+                method: 'GET'
+            }, function (error, response, body) {
+                console.log(body)
+                
+                // const placesJson = JSON.parse(
+                //         convert.xml2json(body, {
+                //         compact: true,
+                //         spaces: 4
+                //     })
+                // )
+                res.send('후보자 공약 조회!')
+                // if (placesJson.response.body === undefined) {
+                //     res.status(500).json({
+                //         message: 'Internal server error'
+                //     })
+                // } else {
+                //     res.status(200).json(placesJson.response.body.items.item)
+                // }
+            });
+        } else {
+            res.status(404).json({
+                message: 'post correct info'
+            })
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
 const port = 5000;
-const opinionRouter = require('./routes/OpinionRouter')
-const mapRouter = require('./routes/mapRouter')
+const opinionRouter = require('./routes/OpinionRouter');
+const mapRouter = require('./routes/mapRouter');
+const promiseRouter = require('./routes/promiseRouter');
 
-app.use(cors())
-app.use('/', opinionRouter)
-app.use('/map', mapRouter)
+app.use(cors());
+app.use('/', opinionRouter);
+app.use('/map', mapRouter);
+app.use('/promises', promiseRouter);
 
 app.listen(port, () => {
     console.log(`server is starting on ${port}`)
