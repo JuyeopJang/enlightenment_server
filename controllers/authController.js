e1ec923ad4205ee4be3e67db4ceaac990c2e9d1b
juyeop <shininggiver@gmail.com>
2021-04-12 19:00:23 +0900

    [feat/googleLogin][구글 로그인 기능 구현]

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const { user } = require('../models')
+const passport = require('passport')
+module.exports = {
+    logout: async (req, res) => {
+
+        // const authorization = req.headers['authorization'];
+        // // 왜 authorization.split을 사용하는지, 왜 1번째 인덱스의 값을 얻는지 console.log를
+        // // 사용해 확인해보세요!
+        // const token = authorization.split(' ')[1];
+        if (req.isAuthenticated()) {
+            req.logout()
+            res.status(200).json({
+                message: 'Successfully logout!'
+            })
+        } else {
+            res.status(401).json({
+                message: 'Invalid User'
+            })
+        }
+    },
+    redirect: async (req, res) => {
+        if (req.user) {
+            res.status(200).json({
+                accessToken: req.user
+            })
+        } else {
+            res.status(401).json({
+                message: 'Invalid User'
+            })
+        }
+    }
+}
--- index.js
+++ index.js
@@ -5,11 +5,63 @@ const port = 5000;
 const opinionRouter = require('./routes/OpinionRouter');
 const mapRouter = require('./routes/mapRouter');
 const promiseRouter = require('./routes/promiseRouter');
+const authRouter = require('./routes/authRouter');
+const session = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { user } = require('./models')
+
+app.use(session({ secret: 'SECRET_CODE', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: "http://localhost:5000/auth/google/redirect"
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    user.findOne({
+      where: {
+        googleId: profile.id
+      }
+    })
+    .then(currentUser => {
+      if (currentUser) {
+        return cb(null, accessToken);
+      } else {
+        user.create({
+          googleId: profile.id
+        })
+        .then(createdUser => {
+          if (createdUser) return cb(null, accessToken);
+        })
+        .catch(err => {
+          if (err) console.log(err)
+        })
+      }
+    })
+    .catch(err => {
+      if (err) console.log(err)
+    })
+  }
+));
+
+passport.serializeUser((accessToken, done) => {
+  done(null, accessToken);
+});
+passport.deserializeUser((accessToken, done) => {
+  // console.log('deserializedUser', user)
+  done(null, accessToken);
+});
 
 app.use(cors());
 app.use('/', opinionRouter);
 app.use('/map', mapRouter);
 app.use('/promises', promiseRouter);
+app.use('/auth', authRouter)
 
 app.listen(port, () => {
     console.log(`server is starting on ${port}`)
