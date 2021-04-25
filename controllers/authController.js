8da6264cd1f81a4925660b624c13a0e9022d9a68
juyeop <shininggiver@gmail.com>
2021-04-26 07:43:56 +0900

    https server

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,21 +22,24 @@ module.exports = {
                 expires: new Date(Date.now() + 24 * 3600000),
                 sameSite: 'none',
                 path: '/',
-                secure: true
+                secure: true,
+                httpOnly: false
             })
             res.cookie('accessToken', req.user.accessToken, {
                 domain: 'kelection.ml',
                 expires: new Date(Date.now() + 24 * 3600000), 
                 sameSite: 'none',
                 path: '/',
-                secure: true    
+                secure: true,
+                httpOnly: false    
             }),
             res.cookie('email', req.user.email, {
                 domain: 'kelection.ml',
                 expires: new Date(Date.now() + 24 * 3600000),
                 sameSite: 'none',
                 path: '/',
-                secure: true
+                secure: true,
+                httpOnly: false
             }),
             // 리다이렉트를 시키기때문에 쿠키로밖에 내려줄 수 없는 상황!
             // userId와 accessToken 이란 이름으로 2개의 쿠키를 만들어 내려줌
